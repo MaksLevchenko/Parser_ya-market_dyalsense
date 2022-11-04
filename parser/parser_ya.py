edf4f9a79592869761c4af1a3395778bee121096
madmax484 <111285984+madmax484@users.noreply.github.com>
2022-11-04 13:55:46 +0300

    Update parser_ya.py

--- parser/parser_ya.py
+++ parser/parser_ya.py
@@ -27,8 +27,8 @@ def get_funk():
         # time.sleep(random.randrange(2, 4))
         driver.implicitly_wait(5)
 
-        for cookie in pickle.load(open('session', 'rb')):
-                driver.add_cookie('YUOR_COOKIE)
+        for cookie in pickle.load(open('YUOR_FILE_WITH_COOKIE', 'rb')):
+                driver.add_cookie(cookie)
         driver.get(url)
         print(driver.window_handles)
         # time.sleep(random.randrange(2, 5))
