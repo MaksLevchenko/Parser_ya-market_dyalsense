2e630526174c6bec77df232b1e7e10b3d3e0cd65
madmax484 <madmax20@bk.ru>
2023-03-17 13:36:43 +0300

    tele_bot 1

--- parser/parser_ya.py
+++ parser/parser_ya.py
@@ -1,13 +1,11 @@
 import json
 from selenium import webdriver
 import pickle
-import time
-import random
 from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
 
-def get_funk():
 
+def get_funk():
     ua = UserAgent()
 
     url = 'https://market.yandex.ru/catalog--ruli-dzhoistiki-geimpady/26908330/list?srnum=672&was_redir=1&rt=9&rs=eJwzEglgrGLheHucdRYjZ0ppYk5xal5xKgBJFwc_&text=dualsense&hid=91117&allowCollapsing=1&local-offers-first=0&pricefrom=3500&priceto=7200&glfilter=7893318%3A152955'
@@ -17,42 +15,31 @@ def get_funk():
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.headless = True
 
-    driver = webdriver.Chrome(r'C:\Users\madma\PycharmProjects\Parser_ya-market_dyalsense\parser\chromedriver.exe',\
+    driver = webdriver.Chrome(r'C:\Users\madma\PycharmProjects\Parser_ya-market_dyalsense\parser\chromedriver.exe', \
                               options=options)
 
-    # reg = requests.get(url=url, headers=headers)
-    # print(reg.text)
     try:
         driver.get(url)
-        # time.sleep(random.randrange(2, 4))
         driver.implicitly_wait(5)
 
-        for cookie in pickle.load(open('YUOR_FILE_WITH_COOKIE', 'rb')):
-                driver.add_cookie(cookie)
+        for cookie in pickle.load(open('session', 'rb')):
+            driver.add_cookie(cookie)
         driver.get(url)
         print(driver.window_handles)
-        # driver.find_element(By.XPATH, "//div[@data-zone-name='continueWeb']").click()
-        # time.sleep(random.randrange(100, 155))
         driver.implicitly_wait(5)
 
         result_data = []
         gamepads = driver.find_elements(By.XPATH, "//img[@data-tid='80e17da7']")
         for gamepad in gamepads:
             gamepad.click()
-            #time.sleep(random.randrange(2, 4))
             driver.implicitly_wait(5)
-
             driver.switch_to.window(driver.window_handles[1])
-            #time.sleep(random.randrange(2, 4))
             driver.implicitly_wait(5)
-
             link = driver.current_url
             name_gamepad = driver.find_element(By.XPATH, "//h1[@data-tid='723082e']")
             print(name_gamepad.text)
-
             price = driver.find_elements(By.XPATH, "//span[@data-auto='mainPrice']")
             print(price[1].text)
-
             data = (
                 {
                     'link': link,
@@ -61,13 +48,11 @@ def get_funk():
                 }
             )
 
-
             result_data.append(data)
 
             driver.close()
 
             driver.switch_to.window(driver.window_handles[0])
-            #time.sleep(random.randrange(2, 5))
             driver.implicitly_wait(5)
 
         with open('data.json', 'w', encoding='utf-8') as file:
@@ -78,8 +63,10 @@ def get_funk():
         driver.close()
         driver.quit()
 
+
 def main():
     get_funk()
+
 
 if __name__ == '__main__':
     main()
--- parser/tele_bot.py
+++ parser/tele_bot.py
@@ -1,12 +1,17 @@
 import json
+import os
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 from parser_ya import get_funk
 from aiogram.utils.markdown import hlink, hbold
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
-bot = Bot(token='YUORE_TOKEN', parse_mode=types.ParseMode.HTML)
+bot = Bot(token=os.getenv('bot_token'), parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
+
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
@@ -15,10 +20,10 @@ async def start(message: types.Message):
     keyboard.add(*start_button)
     await message.answer('Товары со скидкой!', reply_markup=keyboard)
 
+
 @dp.message_handler(Text(equals='Геймпады'))
 async def get_discount(message: types.Message):
     await message.answer('Пожалуйста подождите...')
-
     get_funk()
 
     with open('data.json', 'r', encoding='utf-8') as file:
@@ -32,6 +37,7 @@ async def get_discount(message: types.Message):
 
 def main():
     executor.start_polling(dp)
+
 
 if __name__ == '__main__':
     main()
